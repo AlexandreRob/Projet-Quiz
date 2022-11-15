a47b529418c1d6de6c7815abc84d7717d807b2da
AlexandreRob <alexandre.robert974@hotmail.fr>
2022-11-15 11:01:57 +0400

    Ajout btn + page import csv

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('add_quiz/', views.add_quiz, name="add-quiz"),
     path('upload_quiz/', views.upload_quiz, name="upload-quiz"),
     path('quizz/', views.quizz, name='quizz'),
-    path('list_question', views.all_questions, name='list-question')
+    path('list_question', views.all_questions, name='list-question'),
+    path('import_csv', views.page_csv, name='import-csv')
     # path('service_sessions/', views.service_sessions, name="service-sessions"),
 ]
